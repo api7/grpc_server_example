1bdf115b8fe0cca78b72192dc52301f64a60d90f
nic-chen <johz@163.com>
2019-08-29 14:28:52 +0800

    feat grpc connect pool

--- client.go
+++ client.go
@@ -28,9 +28,26 @@ import (
     "runtime"
     "time"
 
-    pb "github.com/nic-chen/grpc_server_example/proto"
+    "sync"
+
     "google.golang.org/grpc"
+    "google.golang.org/grpc/connectivity"
+
+    pb "github.com/nic-chen/grpc_server_example/proto"
 )
+
+type Pool struct {
+    size int
+    ttl  int64
+
+    sync.Mutex
+    conns map[string][]*poolConn
+}
+
+type poolConn struct {
+    cc      *grpc.ClientConn
+    created int64
+}
 
 const (
     address = "127.0.0.1:50051"
@@ -40,17 +57,23 @@ func main() {
 
     runtime.GOMAXPROCS(1)
 
+    p := NewPool(300, time.Minute)
+
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         r.ParseForm()
         name := r.Form.Get("name")
 
+        //cc, err := p.Get(l.Addr().String(), grpc.WithInsecure())
+
         // Set up a connection to the server.
-        conn, err := grpc.Dial(address, grpc.WithInsecure())
+        conn, err := p.Get(address, grpc.WithInsecure())
         if err != nil {
             log.Fatalf("did not connect: %v", err)
         }
-        defer conn.Close()
-        c := pb.NewGreeterClient(conn)
+
+        defer p.Put(address, conn, nil)
+
+        c := pb.NewGreeterClient(conn.GetCC())
 
         // Contact the server and print out its response.
         if len(os.Args) > 1 {
@@ -59,6 +82,7 @@ func main() {
         ctx, cancel := context.WithTimeout(context.Background(), time.Second)
         defer cancel()
         gr, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+
         if err != nil {
             log.Fatalf("could not greet: %v", err)
         }
@@ -72,4 +96,70 @@ func main() {
     })
 
     http.ListenAndServe(":1210", nil)
+}
+
+func NewPool(size int, ttl time.Duration) *Pool {
+    return &Pool{
+        size:  size,
+        ttl:   int64(ttl.Seconds()),
+        conns: make(map[string][]*poolConn),
+    }
+}
+
+func (p *Pool) Get(addr string, opts ...grpc.DialOption) (*poolConn, error) {
+    p.Lock()
+    conns := p.conns[addr]
+    now := time.Now().Unix()
+
+    // while we have conns check age and then return one
+    // otherwise we'll create a new conn
+    for len(conns) > 0 {
+        conn := conns[len(conns)-1]
+        conns = conns[:len(conns)-1]
+        p.conns[addr] = conns
+
+        // if conn is old or not ready kill it and move on
+        if d := now - conn.created; d > p.ttl || conn.cc.GetState() != connectivity.Ready {
+            conn.cc.Close()
+            continue
+        }
+
+        // we got a good conn, lets unlock and return it
+        p.Unlock()
+
+        return conn, nil
+    }
+
+    p.Unlock()
+
+    // create new conn
+    cc, err := grpc.Dial(addr, opts...)
+    if err != nil {
+        return nil, err
+    }
+
+    return &poolConn{cc, time.Now().Unix()}, nil
+}
+
+func (p *Pool) Put(addr string, conn *poolConn, err error) {
+    // don't store the conn if it has errored
+    if err != nil {
+        conn.cc.Close()
+        return
+    }
+
+    // otherwise put it back for reuse
+    p.Lock()
+    conns := p.conns[addr]
+    if len(conns) >= p.size {
+        p.Unlock()
+        conn.cc.Close()
+        return
+    }
+    p.conns[addr] = append(conns, conn)
+    p.Unlock()
+}
+
+func (pc *poolConn) GetCC() *grpc.ClientConn {
+    return pc.cc
 }
